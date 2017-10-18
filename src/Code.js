b86b07fcebe394ce64098a48425ee63f3bb035e3
Jeremy Bell <jbell0358@gmail.com>
2017-10-18 15:07:40 -0700

    Add javascript template

--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,14 @@
-function myFunction() {
-  
+function onOpen() {
+  SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
+      .createMenu('Custom Menu')
+      .addItem('Show sidebar', 'showSidebar')
+      .addToUi();
+}
+
+function showSidebar() {
+  var html = HtmlService.createHtmlOutputFromFile('user-interface')
+      .setTitle('My custom sidebar')
+      .setWidth(300);
+  SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
+      .showSidebar(html);
 }
